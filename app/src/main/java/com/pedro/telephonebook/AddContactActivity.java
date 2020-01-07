1fee6c0ad42f649c7de82793c29c208fcb251c60
PedroElrik <pedropsarmento@outlook.com>
2020-01-07 16:14:31 -0300

    Update 07/01/2020 commit

--- app/src/main/java/com/pedro/telephonebook/AddContactActivity.java
+++ app/src/main/java/com/pedro/telephonebook/AddContactActivity.java
@@ -1,33 +1,57 @@
 package com.pedro.telephonebook;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.Toolbar;
 
+import com.pedro.telephonebook.Control.ContactCtrl;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class AddContactActivity extends AppCompatActivity {
 
+    ContactCtrl contactCtrl;
+
     Toolbar toolbar;
+    CircleImageView circleImg;
     AppCompatEditText editTextName;
     AppCompatEditText editTextNickName;
     AppCompatEditText editTextTel;
     AppCompatEditText editTextEmail;
+    ImageView btnAddImg;
+
+    static final int GET_IMAGE = 1;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
 
+        contactCtrl = new ContactCtrl();
+
+        circleImg = findViewById(R.id.img_contact);
         editTextName = findViewById(R.id.edit_txt_name);
         editTextNickName = findViewById(R.id.edit_txt_nickname);
         editTextTel = findViewById(R.id.edit_txt_tel);
         editTextEmail = findViewById(R.id.edit_txt_email);
+        btnAddImg = findViewById(R.id.btnAddImg);
 
         toolbar = findViewById(R.id.toolbar_add_contact);
         setSupportActionBar(toolbar);
@@ -38,6 +62,13 @@ public class AddContactActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        btnAddImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
             }
         });
 
@@ -57,9 +88,33 @@ public class AddContactActivity extends AppCompatActivity {
         switch (item.getItemId()){
 
             case R.id.btn_save_contact:
+                contactCtrl.addContact(this, filePath.toString(), editTextName, editTextNickName, editTextTel, editTextEmail);
+                finish();
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showFileChooser(){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, GET_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GET_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null){
+            filePath = data.getData();
+
+            try{
+                circleImg.setImageURI(filePath);
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/pedro/telephonebook/Control/ContactCtrl.java
@@ -0,0 +1,26 @@
+package com.pedro.telephonebook.Control;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.pedro.telephonebook.DataBase.ContactsDAO;
+import com.pedro.telephonebook.Models.Contact;
+
+public class ContactCtrl {
+
+    public void addContact(Context context, String avatar, AppCompatEditText name, AppCompatEditText nickName, AppCompatEditText tel, AppCompatEditText email){
+        ContactsDAO dao = new ContactsDAO(context);
+
+        Contact contact = new Contact();
+        contact.setAvatar(avatar);
+        contact.setName(name.getText().toString());
+        contact.setNickname(nickName.getText().toString());
+        contact.setTel(tel.getText().toString());
+        contact.setEmail(email.getText().toString());
+
+        dao.insertContact(contact);
+    }
+
+}
